c1cba7ebb8c1d28cc53f395fd5e414d5a7d76755
Patrick Carroll <pcarroll@tandtcomputing.com>
2012-11-20 15:40:04 -0500

    please record

---
+++ database/users.sql
@@ -0,0 +1,54 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 15, 2012 at 06:13 AM
+-- Server version: 5.1.37
+-- PHP Version: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARA////CTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(30) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `salt` varchar(3) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pw_assigned` date DEFAULT NULL,
+  `last_login` date DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `login_failure` tinyint(4) NOT NULL,
+  `account_locked` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userId` (`userId`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `userId`, `password`, `salt`, `email`, `pw_assigned`, `last_login`, `username`, `login_failure`, `account_locked`) VALUES
+(14, 'jcarroll', '43c2a8b0e026c68778f189c86e494923093833e83b561687dff00244e8e2578d', '931', 'joyjmc@aol.com', NULL, '2012-11-14', 'Joy McCoy', 2, 'N');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
